1dd97ef941668c348121edd7990fb53882503ee8
Theophile-Kango <fadhili.kango@gmail.com>
2020-06-11 05:49:43 +0200

    Resolved the binary search recursion coding challenge

---
+++ ruby/chocolate_feat.rb
@@ -0,0 +1,10 @@
+def chocolateFeast(n, c, m)
+    b = n/c
+    bonus = (b/m).round
+    mod = (b % m)
+    b += bonus
+     
+    
+    if mod < m 
+    
+end
--- ruby/recursion_sqrt.rb
+++ ruby/recursion_sqrt.rb
@@ -4,6 +4,7 @@ end
 
 def sqrt_recursive(number, min_interval, max_interval)
     # Your code here
+<<<<<<< HEAD
     epsillon = 0.00001
     current_root = (min_interval + max_interval)/2
     current_approx = current_root**2
@@ -20,3 +21,31 @@ end
 
 puts sqrt(1)
 puts sqrt(9)
+=======
+    # epsillon = 0.00001
+    # current_root = (min_interval + max_interval)/2
+    # current_approx = current_root**2
+    # if ((current_approx - number).abs < epsillon)
+    #     return current_root 
+    # else
+    #     if (current_approx > number)
+    #         sqrt_recursive(number, min_interval, current_root)
+    #     else
+    #         sqrt_recursive(number, current_root, max_interval)
+    #     end
+    # end
+
+    return number if (number == 0 || number == 1)
+    min_interval = 1
+    mid = (min_interval + max_interval)/2
+    return mid if (mid**2 <= number) && ((mid + 1)**2 > number)
+    if (mid**2 < number)
+        return sqrt_recursive(mid+1, max_interval, number)
+    else
+        return sqrt_recursive(min_interval, mid-1, number)
+    end
+end
+
+puts sqrt(9)
+puts sqrt(16)
+>>>>>>> Tried the chocolate feat
