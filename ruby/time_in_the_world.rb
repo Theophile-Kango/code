198357e4c4741f5c2334daf7a015da9cf299b8a8
Theophile-Kango <fadhili.kango@gmail.com>
2020-07-21 02:23:12 +0200

    Finish the challenge time in the world

--- ruby/time_in_the_world.rb
+++ ruby/time_in_the_world.rb
@@ -15,12 +15,23 @@ def number_to_word number
 end
 
 def timeInWords(h, m)
-   
+    return "#{number_to_word(h)} o' clock" if m == 0
+    minute = m == 1 ? "minute" : "minutes"
+    if (m > 0) && (m < 30) 
+        return "quarter past #{number_to_word(h)}" if m == 15
+        return "#{number_to_word(m)} #{minute} past #{number_to_word(h)}"
+    elsif (m == 30)
+        return "half past #{number_to_word(h)}"
+    elsif (m > 30) && (m < 60) && (m != 30)
+        m = 60 - m
+        return "quarter to #{number_to_word(h + 1)}" if m == 15
+        return "#{number_to_word(m)} #{minute} to #{number_to_word(h + 1)}"
+    end
 
 end
 
 p number_to_word(5)
-p number_to_word(21)
-p number_to_word(30)
-p number_to_word(55)
-p number_to_word(60)
+p timeInWords(5, 0)
+p timeInWords(5, 1)
+p timeInWords(12, 15)
+p timeInWords(5, 45)
