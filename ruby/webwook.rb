6fb9e10d40a87f1849ce1845181da41d608a037a
Theophile-Kango <fadhili.kango@gmail.com>
2020-07-29 08:49:27 +0200

    Cleaner code

--- ruby/webwook.rb
+++ ruby/webwook.rb
@@ -1,17 +1,13 @@
 def workbook(n, k, arr)
-   answer = 0
-   page = 1
-   for probs in arr
+  answer = 0
+  page = 1
+  for probs in arr
     for index in (1..probs)
-        if index == page
-            answer += 1
-        end
-        if index == probs || index % k == 0
-            page += 1
-        end
+        answer += 1 if index == page
+        page += 1 if index == probs || index % k == 0
     end
-   end
-   return answer
+  end
+  return answer
 end
 
 p workbook(5,3,[4, 2, 6, 1, 10])
