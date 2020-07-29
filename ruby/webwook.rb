12ee3fd32d6f69262b0f458549bc54b35f092233
Theophile-Kango <fadhili.kango@gmail.com>
2020-07-29 08:43:31 +0200

    Correct lisa's webwook challenge

--- ruby/webwook.rb
+++ ruby/webwook.rb
@@ -1,17 +1,17 @@
 def workbook(n, k, arr)
-    count = 0
-    res_arr = []
-    arr.each do |elt|
-        (elt/k).times do |val|
-            res_arr << k + (k * val)
+   answer = 0
+   page = 1
+   for probs in arr
+    for index in (1..probs)
+        if index == page
+            answer += 1
         end
-        res_arr << elt if (elt % k) != 0
+        if index == probs || index % k == 0
+            page += 1
+        end
     end
-    count += 1 if (res_arr[0] >= 1)
-    res_arr.length.times do |i|
-        count += 1 if (res_arr[i] >= (i + 1) && res_arr[i - 1] < (i + 1))
-    end
-    count
+   end
+   return answer
 end
 
 p workbook(5,3,[4, 2, 6, 1, 10])
