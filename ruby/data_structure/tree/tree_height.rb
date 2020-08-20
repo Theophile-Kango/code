393be7cebb989b7a9447ecb1f5cff77e26904a53
Theophile-Kango <fadhili.kango@gmail.com>
2020-08-20 17:34:49 +0200

    tree_hteight iterative method

---
+++ ruby/data_structure/tree/tree_height.rb
@@ -0,0 +1,28 @@
+def tree_height(tree_as_list)
+   #0 Declare a heights array which will store the heights of each sub tree
+  heights = []
+  #1 Iterate through the tree_array starting from last element down to first
+  (tree_as_list.length - 1).downto(0) do |i|
+  
+  #2 For each element, find initial height
+  initial_height = tree_as_list[i] == 0 ? 0 : 1
+  
+  # 3 Find height of left child
+  left_child_index = tree_as_list[2*i + 1].nil? ? nil : heights.length - i - 1 #index of left child's height in heights
+  left_child_height = left_child_index.nil? ? 0 : heights[left_child_index] 
+  
+  # 4 Find height of right child
+  right_child_index = tree_as_list[2*i + 2].nil? ? nil : left_child_index - 1 #index of right child's height in heights
+  right_child_height = right_child_index.nil? ? 0 : heights[right_child_index]
+  
+  # 5 Find element's total height
+  total_height = initial_height + [left_child_height,right_child_height].max
+    
+  # 6 Push total height to heights array
+  heights.push(total_height)
+    
+ end
+ puts heights[-1]
+end
+
+puts tree_height([2, 7, 5, 2, 6, 0, 9])
