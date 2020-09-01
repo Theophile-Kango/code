28ea1e089958f9e1f24f24af577952098fa68fef
Theophile-Kango <fadhili.kango@gmail.com>
2020-09-01 10:01:44 +0200

    Add the depth_first_search algorithm

---
+++ ruby/data_structure/trees_graphs/deth_first_search.rb
@@ -0,0 +1,31 @@
+
+def depth_first_search(graph)
+    # write your code here
+    stack = [0]
+    result = []
+    visited = []
+  
+    until stack.empty?
+      p stack
+      head = stack.pop
+      result << head unless visited.include? head
+      visited << head
+      return result if graph[head].nil?
+  
+      not_visited = graph[head].reject { |node| visited.include? node }
+  
+      stack += not_visited.reverse
+    end
+  
+    result
+end
+
+p depth_first_search({
+    0 => [2], 
+    1 => [4], 
+    2 => [5, 0, 3], 
+    3 => [2], 
+    4 => [1, 5], 
+    5 => [4, 2]
+  })
+# => [0, 2, 5, 4, 1, 3]
