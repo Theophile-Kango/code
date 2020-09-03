45561e51c1482f4922f68780518c73792c6ebc93
Theophile-Kango <fadhili.kango@gmail.com>
2020-09-03 11:57:59 +0200

    Implement the procedure solution

--- ruby/data_structure/trees_graphs/bread_first.rb
+++ ruby/data_structure/trees_graphs/bread_first.rb
@@ -1,20 +1,41 @@
+# def bfs(graph)
+#     # write your code here
+#     array = [0]
+#     connected_node(array, graph, 0)
+# end
+  
+# def connected_node(array, hash, index)
+#     return nil if array.size >= hash.size || hash[index].nil?
+  
+#     nodes = hash[index].reject { |i| array.include?(i) }
+  
+#     array << nodes
+#     array.flatten!
+#     connected_node(array, hash, nodes[0]) unless nodes.nil?
+#     connected_node(array, hash, nodes[1]) if nodes.size > 1
+  
+#     array
+# end
+
 def bfs(graph)
-    # write your code here
-    array = [0]
-    connected_node(array, graph, 0)
-end
-  
-def connected_node(array, hash, index)
-    return nil if array.size >= hash.size || hash[index].nil?
-  
-    nodes = hash[index].reject { |i| array.include?(i) }
-  
-    array << nodes
-    array.flatten!
-    connected_node(array, hash, nodes[0]) unless nodes.nil?
-    connected_node(array, hash, nodes[1]) if nodes.size > 1
-  
-    array
+   discovered  = Array.new(graph.keys.length, false)
+   queue = []
+   path = []
+
+   discovered[0] = true
+   queue << 0
+
+   until queue.empty?
+    current = queue.shift
+    path << current 
+    graph[current].each do |adjacent|
+        unless discovered[adjacent]
+            queue << adjacent
+            discovered[adjacent] = true
+        end
+    end
+   end
+   path
 end
 
 p bfs({
