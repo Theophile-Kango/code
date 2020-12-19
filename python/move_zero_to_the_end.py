3765c29d9dfbfb98945743ebb4c16e02f07f34ee
Theophile-Kango <fadhili.kango@gmail.com>
2020-12-19 15:41:13 +0200

    Add the best code ware solution

--- python/move_zero_to_the_end.py
+++ python/move_zero_to_the_end.py
@@ -1,9 +1,5 @@
 def move_zeros(array):
-    zero_list = list(
-        filter(lambda a: a == 0 and not isinstance(a, bool), array))
-    result = list(
-        filter(lambda a: a != 0 or isinstance(a, bool), array)) + zero_list
-    return result
+    return sorted(array, key=lambda x: x == 0 and type(x) is not bool)
 
 
 print(move_zeros([0, 1, None, 2, False, 1, 0]))
