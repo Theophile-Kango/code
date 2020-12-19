31449ae29a22b0ec0a17d85165c5aeb068a08ef2
Theophile-Kango <fadhili.kango@gmail.com>
2020-12-19 11:52:40 +0200

    Implement the likes solution from codeware challenge

---
+++ python/likes.py
@@ -0,0 +1,17 @@
+def likes(names):
+
+    d = {
+        0: "no one likes this",
+        1: "{} likes this",
+        2: "{} and {} like this",
+        3: "{}, {} and {} like this",
+        4: "{}, {} and {others} others like this"
+    }
+    length = len(names)
+
+    return d[min(4, length)].format(*names, others=length - 2)
+
+
+names = ['Theophile', 'Bello', 'Murilo']
+
+print(likes(names))
