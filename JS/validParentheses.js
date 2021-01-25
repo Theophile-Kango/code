e7fb2f26092768817f79b53d438604be9dde66bc
Theophile-Kango <fadhili.kango@gmail.com>
2021-01-25 09:28:31 +0200

    Add the efficient solution for the valid parentheses challenge

--- JS/validParentheses.js
+++ JS/validParentheses.js
@@ -1,15 +1,22 @@
 function validParentheses(parens) {
-  if (parens == "") return false;
-  return !parens.split("").reduce((uptoPrevChar, thisChar) => {
-    if (thisChar === "(") {
-      return ++uptoPrevChar;
-    } else if (thisChar === ")") {
-      return --uptoPrevChar;
-    }
+  // return !parens.split("").reduce((uptoPrevChar, thisChar) => {
+  //   if (thisChar === "(") {
+  //     return ++uptoPrevChar;
+  //   } else if (thisChar === ")") {
+  //     return --uptoPrevChar;
+  //   }
 
-    return uptoPrevChar;
-  }, 0);
+  //   return uptoPrevChar;
+  // }, 0);
+
+  let length = parens.length / 2;
+
+  for (let i = 0; i <= length; ++i) {
+    parens = parens.replace("()", "");
+  }
+
+  return parens == "";
 }
 
-console.log(validParentheses(""));
+console.log(validParentheses("(())((()())())"));
 console.log(validParentheses("())"));
