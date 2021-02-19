19641c757fe1c815002dbba00779997e95252ecf
Theophile-Kango <fadhili.kango@gmail.com>
2021-02-19 14:24:12 +0200

    Resolved the square root leetcode coding challenge

---
+++ JS/sqrt.js
@@ -0,0 +1,18 @@
+var mySqrt = function (x) {
+  if (x < 2) return x;
+  let left = 1;
+  let right = x;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (mid * mid === x) {
+      return mid;
+    } else if (mid * mid > x) {
+      right = mid;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    }
+  }
+  return left - 1;
+};
+
+console.log(mySqrt(10));
