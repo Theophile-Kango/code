1f7eb46df969a8427147b8c945c808e4af85eff1
Theophile-Kango <fadhili.kango@gmail.com>
2021-03-26 07:25:06 +0200

    Resolved the fibonachi function js recursively

--- JS/fibonachi.js
+++ JS/fibonachi.js
@@ -1,17 +1,5 @@
 function fibonachi(num){
-    current = 0, index = 0
-    arr = [];
-    
-    while (index < num){
-        index += 1
-        arr.push(index)
-    } 
-    let arr2 = [];
-    arr.map((item) => { 
-        // Increment each item by 1
-         arr2.push(arr[item+ arr[i]]); 
-    });
-     return arr2;   
+    return (num < 2) ? 1 : fibonachi(num - 1) + fibonachi(num - 2)
 }
 
 
