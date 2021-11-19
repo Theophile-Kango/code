7ff0a525a1693a610d2112352325cd0e79ed169b
Theophile Kango <fadhili.kango@gmail.com>
2021-11-19 21:13:45 +0200

    Resolved the count Invention algo from codewar

---
+++ JS/countInvention.js
@@ -0,0 +1,15 @@
+const countInvention = (array) => {
+    //buble sort algorithm
+    let count = 0;
+    for(let i = 0; i < array.length; i++){
+        for(let j = i+1; j<array.length; j++){
+            if(array[i] > array[j]){
+                [array[i], array[j]] = [array[j], array[i]];
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
+
+console.log(countInvention([10,3,5,1,4]))
