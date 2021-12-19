647b5c3b8f0f580649b4c5d6a0c4a411accd166d
Theophile Kango <fadhili.kango@gmail.com>
2021-12-19 10:38:25 +0200

    Resolved Kin digital challenges

--- JS/countInvention.js
+++ JS/countInvention.js
@@ -3,13 +3,14 @@ const countInvention = (array) => {
     let count = 0;
     for(let i = 0; i < array.length; i++){
         for(let j = i+1; j<array.length; j++){
-            if(array[i] > array[j]){
-                [array[i], array[j]] = [array[j], array[i]];
-                count += 1;
-            }
+            // if(array[i] > array[j]){
+            //     [array[i], array[j]] = [array[j], array[i]];
+            //     count += 1;
+            // }
+            console.log(`${i} ${j}`)
         }
     }
-    return count;
+    //return count;
 }
 
-console.log(countInvention([10,3,5,1,4]))
+countInvention([10,3,5,1,4])
