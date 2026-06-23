aeac6568e996232799c55bc0ba0ccd608e22deac
Theophile-Kango <fadhili.kango@gmail.com>
2026-06-23 15:56:46 +0200

    Resolved int_to_roman challenge

---
+++ ruby/algorithm/int_to_roman.rb
@@ -0,0 +1,32 @@
+def int_to_roman(num)
+  map = {
+    1000 => "M",
+    900  => "CM",
+    500  => "D",
+    400  => "CD",
+    100  => "C",
+    90   => "XC",
+    50   => "L",
+    40   => "XL",
+    10   => "X",
+    9    => "IX",
+    5    => "V",
+    4    => "IV",
+    1    => "I"
+  }
+
+  result = ""
+
+  map.each do |value, symbol|
+    while num >= value
+      result << symbol
+      num -= value
+    end
+  end
+
+  result
+
+end
+
+num = 3749
+p int_to_roman(num)
